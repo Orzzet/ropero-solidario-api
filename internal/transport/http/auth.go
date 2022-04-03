92eb3a410a52a48a6b0e8f19f01c12c9c9217761
Adrián López Carrillo <adrian.lc92@gmail.com>
2022-04-03 16:03:19 +0200

    feat: correct error status code on auth

--- internal/transport/http/auth.go
+++ internal/transport/http/auth.go
@@ -16,23 +16,23 @@ func (h *Handler) CreateToken(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var credentials models.Credentials
 	if err := json.NewDecoder(r.Body).Decode(&credentials); err != nil {
-		fmt.Fprintf(w, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 	}
 
 	hashedPassword, err := h.Service.GetUserHashedPassword(credentials.Email)
 
 	if err != nil {
-		// If there is an issue with the database, return a 500 error
-		fmt.Fprintf(w, err.Error())
-		w.WriteHeader(http.StatusInternalServerError)
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
 	// Compare the stored hashed password, with the hashed version of the password that was received
 	if err = bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(credentials.Password)); err != nil {
 		// If the two passwords don't match, return a 401 status
-		fmt.Fprintf(w, err.Error())
 		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
@@ -43,8 +43,8 @@ func (h *Handler) CreateToken(w http.ResponseWriter, r *http.Request) {
 	// Sign and get the complete encoded token as a string using the secret
 	tokenString, err := token.SignedString([]byte(h.Secret))
 	if err != nil {
-		fmt.Fprintf(w, err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
 		return
 	}
 
